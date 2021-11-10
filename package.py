e05fe7d8c7290114411dfff6a5b4fc705f28e3c8
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2021-11-10 09:55:49 +0100

    add devtoolset as build_requires

--- package.py
+++ package.py
@@ -6,6 +6,16 @@ variants = [
     ["platform-linux"]
 ]
 
+@early()
+def build_requires():
+    # check if the system gcc is too old <9
+    # then we require devtoolset-9
+    from subprocess import check_output
+    valid = check_output(r"expr `gcc -dumpversion | cut -f1 -d.` \>= 9 || true", shell=True).strip().decode() == "1"
+    if not valid:
+        return ["devtoolset-9"]
+    return []
+
 requires = [
     "jpegturbo-2"
 ]
