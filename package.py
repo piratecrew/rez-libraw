534693eb9747c98d785ba654f0146041906a7dd2
Fredrik Brännbacka <fredrik.brannbacka@ilpvfx.com>
2026-08-10 11:48:14 +0200

    chore: move to gcc package as a private_build_requires

--- package.py
+++ package.py
@@ -6,16 +6,9 @@ variants = [
     ["platform-linux"]
 ]
 
-@early() # type: ignore
-def build_requires():
-    # check if the system gcc is too old <9
-    # then we require devtoolset-9
-    from subprocess import check_output
-    gcc_major = int(check_output(r"gcc -dumpversion | cut -f1 -d.", shell=True).strip().decode())
-    if gcc_major < 9:
-        return ["devtoolset-9"]
-
-    return []
+private_build_requires = [
+    "gcc-11"
+]
 
 requires = [
     "jpegturbo-2"
